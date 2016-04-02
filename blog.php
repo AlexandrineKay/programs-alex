a9ef1956a3ab57d1ad1ce41ee4654da13ad99301
Alexandra <saschaboo@yandex.com>
2016-04-02 16:00:11 +0300

    init

--- blog.php
+++ blog.php
@@ -1,4 +1,6 @@
 <?php
+require "functions.php";
+ini_set('display_errors', 1);
 error_reporting(E_ALL);
 $connection = new \PDO("mysql:host=localhost;dbname=epic", 'root', 'vagrant', [
     \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
@@ -14,43 +16,9 @@ if (!empty($_GET['message'])) {
     ]);
 }
 $messages = $connection->query('SELECT p.`title`,p.`date`,p.`message` FROM `posts`  p LEFT JOIN `users`  u ON p.`user_id`= u.`id` ORDER BY  p.`date` DESC')->fetchAll();
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Blog</title>
-    <style type="text/css">
-        body {
-            width: 60%;
-            margin-left: auto;
-            margin-right: auto;
-        }
-        .left {
-            float: left;
-        }
-        .right {
-            float: right;
-        }
-        .message {
-            margin-bottom: 20px;
-        }
-    </style>
-</head>
+echo template("verstka.php",[
+'messages' => $messages,]);
 
-<?php if (!empty($messages)): ?>
-    <?php foreach ($messages as $message): ?>
-        <div class="message">
-            <div><?= $message['title']; ?></div>
-            <span class="left"><?= htmlspecialchars($message['message']); ?></span>
-            <span class="right"><?= "Добавлено:  ".$message['date']; ?></span>
-        </div>
-        <br/>
-    <?php endforeach ?>
-<?php endif ?>
-<form action="blog.php" method="GET">
-    <input type="text" name="title">
-    <input type="text" name="message">
-    <input type="submit">
-</form>
+
+
 
