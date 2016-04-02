b28a6e7fd5d61d6c3df4e13556c5e3e50c7a1ed7
Alexandra <saschaboo@yandex.com>
2016-04-02 15:13:04 +0300

    init

--- login.php
+++ login.php
@@ -9,9 +9,9 @@ session_start();
 ini_set('display_errors', 1);
 require "functions.php";
 $connection = connection(['host' => 'localhost', 'dbname' => 'epic', 'user' => 'root', 'password' => 'vagrant', 'encoding' => 'utf8']);
-$token = token();
 
-if (!empty($_POST['login'])) {
+
+if (!empty($_POST['login']) &&$_REQUEST['token'] == $_SESSION['token']) {
     $a = $connection->prepare('SELECT * FROM `users` WHERE `login` = :login AND `password`=:password');
     $a->execute([
             ':login' => $_POST['login'],
@@ -27,6 +27,6 @@ if (!empty($_POST['login'])) {
 <form action="login.php" method="POST">
     <input type="text" name="login">
     <input type="password" name="password">
-    <input type ="hidden" name ="token" value = <?php echo $token ?>>
+    <input type ="hidden" name ="token" value = <?php echo token() ?>>
     <input type="submit">
 </form>
