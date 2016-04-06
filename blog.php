febde12b4aa6fb2d0c5d986202c893c636ee5cf8
Alexandra <saschaboo@yandex.com>
2016-04-06 22:23:53 +0300

    init

--- blog.php
+++ blog.php
@@ -1,8 +1,10 @@
 <?php
+session_start();
 require "functions.php";
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 $connection = connection(['host' => 'localhost', 'dbname' => 'epic', 'user' => 'root', 'password' => 'vagrant', 'encoding' => 'utf8']);
+$user = $_SESSION['user'];
 
 if (!empty($_POST['message'])) {
     $a = $connection->prepare("INSERT INTO `posts` SET `message`=:message, `title` = :title,`date`=NOW(), `user_id`='user_id' ");
@@ -17,7 +19,9 @@ $query->execute([
 ':message' => $_POST['message'],
  ':title' =>  $_POST['title'],
 ]);}
-$messages = $connection->query('SELECT p.`title`,p.`date`,p.`message` FROM `posts`  p LEFT JOIN `users`  u ON p.`user_id`= u.`id` ORDER BY  p.`date` DESC')->fetchAll();
+if (empty($user)) {
+   header("Location:login.php" );}
+$messages = $connection->query('SELECT p.`title`,p.`date`,p.`message` FROM `posts`  p   ORDER BY  p.`date` DESC' )->fetchAll();
 echo template("verstka.php",[
 'messages' => $messages,]);
 
