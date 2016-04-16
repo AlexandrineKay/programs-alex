4e6cdd2b7ffb29a3c5f5b3516de0233f52f8d394
Alexandra <saschaboo@yandex.com>
2016-04-16 14:56:52 +0300

    init

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 session_start();
 require "functions.php";
-require "Login.php";
-require "Post.php";
-require "Home.php";
+//require "Login.php";
+//require "Post.php";
+//require "Home.php";
+spl_autoload_register(function ($class)
+{
+    include "{$class}.php";
+});
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 $connection = connection(['host' => 'localhost', 'dbname' => 'epic', 'user' => 'root', 'password' => 'vagrant', 'encoding' => 'utf8']);
