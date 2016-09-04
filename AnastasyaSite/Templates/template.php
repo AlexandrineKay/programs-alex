75c9002d5cab9b475f516ac9a5292799f186fb02
Alexandra <saschaboo@yandex.com>
2016-09-04 23:26:12 +0300

    init

---
+++ AnastasyaSite/Templates/template.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ласточка</title>
+    <link rel="shortcut icon" href="https://66.media.tumblr.com/2e5926fbce178b89b5448e2dc2a2d144/tumblr_oczwn2yvmU1szmf1so1_75sq.png" type="image/png">
+    <style type="text/css">
+        body {background-image: url(http://365psd.ru/images/backgrounds/old-paper-text.jpg);
+            background-color: #c7b39b;
+            font-family:arial, verdana, sans-serif;
+            font-size: 16px;}
+        table{width: 94%; height: 100%; border-collapse: collapse; margin: auto;}
+        table td{height: 50px; border: 0px solid #000;}
+        H1{color: #8A0808;
+            font-style: italic;
+            font-size: 36px;
+            letter-spacing: 10px;}
+        H2{padding: 10px;
+            color: #8A2908;
+            font-size: 24px;}
+        H3{color:#61380B;
+            font-size: 22px;}
+        H4{color:#8A0808;}
+        p {text-indent: 20px;
+            padding: 10px;}
+    </style>
+</head>
+<body>
+<table style = "background-image:url(http://365psd.ru/images/backgrounds/paper-1.jpg);">>
+    <tr>
+       <td><img src="https://67.media.tumblr.com/41bc2deea7a3611f5206de875725737e/tumblr_oczvy4Sgs81szmf1so1_250.png"></td>
+        <td colspan = "2" align = "center">  <H1>  <a name ="nw"></a> Мастерская "Ласточка" </H1> </td>
+    </tr>
+    <tr style = "background-image:url(http://365psd.ru/images/backgrounds/paper-1.jpg);">
+        <td style = "width:20%;height:250px; vertical-align: top;">
+            <table width = "100%" height = "200px" cellpadding="10">
+                <tr><td><H3> Категории </H3> <hr/> </td></tr>
+                <tr><td><H3> На заказ </H3> <hr/> </td></tr>
+                <tr><td><H3> Отзывы </H3> <hr/> </td></tr>
+                <tr><td><H3> "Зал славы" </H3> <hr/> </td></tr>
+                <tr><td><H3> Контакты </H3> <hr/> </td></tr>
+            </table> </td>
+        <td style = "height:525px;"><H2 align = "center">  Новости </H2>
+    <tr>
+        <td colspan = "2" align = "center"> <H4> <b> (c) Коновалова Александра 2016 </b> </H4> </td>
+    </tr>
+</table>
+</body>
+</html>
