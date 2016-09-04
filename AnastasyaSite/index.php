00c03e3c9236b1634cd8aa9f0c2feeb4f65bb44b
Alexandra <saschaboo@yandex.com>
2016-09-04 22:57:15 +0300

    init

---
+++ AnastasyaSite/index.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+require "functions.php";
+spl_autoload_register(function ($class)
+{
+    include "Controllers/{$class}.php";
+});
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+$connection = connection(['host' => 'localhost', 'dbname' => 'nastyashe', 'user' => 'root', 'password' => 'vagrant', 'encoding' => 'utf8']);
+routes($_SERVER['REQUEST_URI'],[
+    'home' => 'Home',
+    'post' => 'Post',
+    'login' => 'Login',
+]);
